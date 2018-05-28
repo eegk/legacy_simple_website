b0c07d3c62f1b489d63b9deab0057373ccc98323
Edgar E. G. Kozlova <eegonzalezk@gmail.com>
2018-05-27 22:56:42 -0300

    Create about.php

---
+++ about.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html PUBLIC "-//eegk//DTD XHTML 1.0 Strict//EN" "eegk">
+
+<html xmlns="eegk template" xml:lang="en" lang="en">
+
+<head>
+
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+
+<meta name="description" content="" />
+
+<meta name="keywords" content="" />
+
+<meta name="author" content="" />
+
+<link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+
+<title>Edgar E. G. Kozlova, Ph.D. Bioinformatics.</title>
+
+</head>
+
+	<body>
+
+		<div id="wrapper">
+
+<?php include('includes/header.php'); ?>
+
+<?php include('includes/nav.php'); ?>
+
+<div id="content">
+
+<h3>About Me</h3>
+
+<p>
+
+Biologist. 
+
+</p>
+
+</div> <!-- end #content -->
+
+<?php include('includes/sidebar.php'); ?>
+
+<?php include('includes/footer.php'); ?>
+
+		</div> <!-- End #wrapper -->
+
+	</body>
+
+</html>
